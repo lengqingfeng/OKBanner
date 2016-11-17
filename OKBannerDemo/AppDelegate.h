361eabf72d806480a3bc317bf1747ba92958b4d2
allen <379306634@qq.com>
2016-11-17 22:28:43 +0800

    Initial Commit

---
+++ OKBannerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OKBannerDemo
+//
+//  Created by lengshengren on 16/11/17.
+//  Copyright © 2016年 Lengshengren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
